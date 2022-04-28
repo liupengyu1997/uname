07fb24720bbba6377b7f8f2c7fe677eb0befa521
liupengyu1 <liupengyu1@jd.com>
2022-04-28 14:27:52 +0800

    第一次

--- src/test.java
+++ src/test.java
@@ -7,24 +7,27 @@ import java.util.concurrent.*;
 
 public class test {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-       test.multi();
-       test.signal();
+        test.multi();
+        test.signal();
+        System.out.println(1);
 
     }
-    static void handle(Integer id){
-        for(int i=0;i<10000;i++){
-            int s=0;
+
+    static void handle(Integer id) {
+        for (int i = 0; i < 10000; i++) {
+            int s = 0;
         }
     }
+
     static void multi() throws ExecutionException, InterruptedException {
         List<Integer> list = new LinkedList<>();
         List<String> res = new LinkedList<>();
         ExecutorService executorService = Executors.newFixedThreadPool(6);
-        List< Future<String> > futures = new LinkedList<>();
-        Long start=System.currentTimeMillis();
-        for(int i=0;i<4;i++){
-            int id=i;
-            Future<String> future=executorService.submit(new Callable<String>() {
+        List<Future<String>> futures = new LinkedList<>();
+        Long start = System.currentTimeMillis();
+        for (int i = 0; i < 4; i++) {
+            int id = i;
+            Future<String> future = executorService.submit(new Callable<String>() {
                 @Override
                 public String call() throws Exception {
                     handle(1);
@@ -33,24 +36,25 @@ public class test {
             });
             futures.add(future);
         }
-        for (Future<String> future:futures){
+        for (Future<String> future : futures) {
             res.add(future.get());
         }
         executorService.shutdown();
-        Long end=System.currentTimeMillis();
-        System.out.println("多线程耗时:"+(end-start));
+        Long end = System.currentTimeMillis();
+        System.out.println("多线程耗时:" + (end - start));
     }
-    static void signal(){
+
+    static void signal() {
         List<Integer> list = new LinkedList<>();
         List<String> res = new LinkedList<>();
         ExecutorService executorService = Executors.newFixedThreadPool(6);
-        List< Future<String> > futures = new LinkedList<>();
-        Long start=System.currentTimeMillis();
-        for(int i=0;i<4;i++){
-           test.handle(1);
+        List<Future<String>> futures = new LinkedList<>();
+        Long start = System.currentTimeMillis();
+        for (int i = 0; i < 4; i++) {
+            test.handle(1);
         }
 
-        Long end=System.currentTimeMillis();
-        System.out.println("单线程耗时:"+(end-start));
+        Long end = System.currentTimeMillis();
+        System.out.println("单线程耗时:" + (end - start));
     }
 }
